e9a34c7809a099e558119be25790150c8207c4c3
Arsen Buhaienko <arsenbuhaienko@gmail.com>
2024-03-16 17:03:41 +0200

    fix

--- HomeWorks/DZ16/SingletonClass.ts
+++ HomeWorks/DZ16/SingletonClass.ts
@@ -1,16 +1,28 @@
-import { Page, firefox } from "@playwright/test"
+import { Page, firefox, chromium, webkit } from "@playwright/test"
 
 export class Singleton{
     private static demoPage: Page
     private constructor(){}
 
-    public static async getPage(){
-        
+    public static async getPageFireFox(){
             const browser = await firefox.launch();
             const context = await browser.newContext();
             const page = await context.newPage();
             Singleton.demoPage = page
-        
             return Singleton.demoPage;
+    }
+    public static async getPageChromium(){
+        const browser = await chromium.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        Singleton.demoPage = page
+        return Singleton.demoPage;
+}
+    public static async getPageWebkit(){
+        const browser = await webkit.launch();
+        const context = await browser.newContext();
+        const page = await context.newPage();
+        Singleton.demoPage = page
+        return Singleton.demoPage;
     }
 }
--- HomeWorks/DZ16/SingletonTest.spec.ts
+++ HomeWorks/DZ16/SingletonTest.spec.ts
@@ -1,8 +1,17 @@
 import { Singleton } from "./SingletonClass";
 import { test } from '@playwright/test';
 
-test('Check Singleton Class', async ({}) => {
-    const page = await Singleton.getPage(); // Получение контекста с класса Сингелтон
+test('Check Singleton Class FireFox', async ({}) => {
+    const page = await Singleton.getPageFireFox(); // Получение контекста с класса Сингелтон
     await page.goto("https://demoqa.com")
-    
+  });
+
+  test('Check Singleton Class Chromium', async ({}) => {
+    const page = await Singleton.getPageChromium(); // Получение контекста с класса Сингелтон
+    await page.goto("https://demoqa.com")
+  });
+
+  test('Check Singleton Class Webkit', async ({}) => {
+    const page = await Singleton.getPageWebkit(); // Получение контекста с класса Сингелтон
+    await page.goto("https://demoqa.com")
   });
